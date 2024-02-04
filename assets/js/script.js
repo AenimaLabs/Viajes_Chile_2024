65a9e888720f4e99b8f04bb5bea71b0d8173bf78
JorgeAndaur <aenimalabs@gmail.com>
2024-02-04 17:27:05 -0300

    footer terminado, js listo

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    
+    $("a").click(function(e){
+        var gato =this.hash
+        $("html, body").animate({
+            scrollTop: $(gato).offset().top -56
+        }, 300);
+    })
+})
+
+// $("a").click(function (event) {
+
+//     if (this.hash !== "") {
+
+//         event.preventDefault();
+
+//         var hash = this.hash;
+
+//         $("html, body").animate({
+//             scrollTop: $(hash).offset().top
+//         }, 100); // primero probar con 0 y aumente de 1000 en 1000 para ver diferencia
+//     }
+// });
+
+// $(document).ready(function () {
+//     $("a").on('click', function (event) {
+//         if (this.hash !== "") {
+//             event.preventDefault();
+//             var hash = this.hash;
+//             $('html, body').animate({
+//                 scrollTop: $(hash).offset().top
+//             }, 1000, function () {
+//                 window.location.hash = hash;
+//             });
+//         }
+//     });
+// });
