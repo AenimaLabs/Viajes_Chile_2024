6463e4fe4c04ac108fab70440ef8c7ef612f6a62
JorgeAndaur <aenimalabs@gmail.com>
2024-02-05 21:22:11 -0300

    formateo js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-    
-    $("a").click(function(e){
-        var gato =this.hash
+$(document).ready(function () {
+
+    $("a").click(function (e) {
+        var gato = this.hash
         $("html, body").animate({
-            scrollTop: $(gato).offset().top -56
+            scrollTop: $(gato).offset().top - 56
         }, 300);
     })
 })
